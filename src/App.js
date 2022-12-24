2dd7e366797a03040d048bde235a5e5f0a62fdcf
Ansh Mondal <ansh1mondal@gmail.com>
2022-12-24 18:49:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import Home from "../src/Components/js/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
---
+++ src/Components/js/Home.jsx
@@ -0,0 +1,192 @@
+import React from "react";
+import "../css/Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="clouds">
+          <div className="cloud1"></div>
+          <div className="cloud2"></div>
+          <div className="cloud3"></div>
+          <div className="cloud4"></div>
+          <div className="cloud5"></div>
+          <div className="cloud6"></div>
+          <div className="cloud7">
+            <div className="gift1">&#127873;</div>
+            <div className="gift2">
+              <img src={require("../gift.png")} alt="gift" />
+            </div>
+            <div className="gift3">
+              <img src={require("../gift-box.png")} alt="gift" />
+            </div>
+            <div className="gift4">
+              <img src={require("../giftbox.png")} alt="gift" />
+            </div>
+            <div className="gift5">
+              <img src={require("../gift.png")} alt="gift" />
+            </div>
+          </div>
+        </div>
+        <div className="Ctree">
+          <div className="treeStar">&#127775;</div>
+          <div className="Leaf1">
+            <div className="leafbush1">
+              <div className="light">
+                <div className="L1"></div>
+                <div className="L2"></div>
+                <div className="L3"></div>
+                <div className="L4"></div>
+                <div className="L5"></div>
+              </div>
+            </div>
+          </div>
+          <div className="Leaf2">
+            <div className="leafbush2">
+              <div className="light">
+                <div className="L1"></div>
+                <div className="L2"></div>
+                <div className="L3"></div>
+                <div className="L4"></div>
+                <div className="L5"></div>
+              </div>
+            </div>
+          </div>
+          <div className="Leaf3">
+            <div className="leafbush3">
+              <div className="light">
+                <div className="L1"></div>
+                <div className="L2"></div>
+                <div className="L3"></div>
+                <div className="L4"></div>
+                <div className="L5"></div>
+              </div>
+            </div>
+          </div>
+          <div className="Leaf4">
+            <div className="leafbush4">
+              <div className="light">
+                <div className="L1"></div>
+                <div className="L2"></div>
+                <div className="L3"></div>
+                <div className="L4"></div>
+                <div className="L5"></div>
+              </div>
+            </div>
+          </div>
+          <div className="treeTrunk">
+            <div className="trunk"></div>
+          </div>
+        </div>
+        <div className="Deer1">
+          <div className="DeerHead">
+            <div className="Faceup">
+              <div className="headeyes">
+                <div className="Seye1"></div>
+                <div className="Seye2"></div>
+              </div>
+            </div>
+            <div className="Facedown">
+              <div className="headnose"></div>
+            </div>
+          </div>
+          <div className="DeerBody"></div>
+          <div className="DeerEars">
+            <div className="ear1"></div>
+            <div className="ear2"></div>
+          </div>
+          <div className="DeerTail"></div>
+          <div className="DeerEyes"></div>
+          <div className="Antelopes">
+            <div className="Ant1">f</div>
+            <div className="Ant2">f</div>
+          </div>
+        </div>
+        <div className="Deer2">
+          <div className="DeerHead">
+            <div className="Faceup">
+              <div className="headeyes">
+                <div className="Seye1"></div>
+                <div className="Seye2"></div>
+              </div>
+            </div>
+            <div className="Facedown">
+              <div className="headnose"></div>
+            </div>
+          </div>
+          <div className="DeerBody"></div>
+          <div className="DeerEars">
+            <div className="ear1"></div>
+            <div className="ear2"></div>
+          </div>
+          <div className="DeerTail"></div>
+          <div className="DeerEyes"></div>
+          <div className="Antelopes">
+            <div className="Ant1">f</div>
+            <div className="Ant2">f</div>
+          </div>
+        </div>
+        <div className="Deer3">
+          <div className="DeerHead">
+            <div className="Faceup">
+              <div className="headeyes">
+                <div className="Seye1"></div>
+                <div className="Seye2"></div>
+              </div>
+            </div>
+            <div className="Facedown">
+              <div className="headnose"></div>
+            </div>
+          </div>
+          <div className="DeerBody"></div>
+          <div className="DeerEars">
+            <div className="ear1"></div>
+            <div className="ear2"></div>
+          </div>
+          <div className="DeerTail"></div>
+          <div className="DeerEyes"></div>
+          <div className="Antelopes">
+            <div className="Ant1">f</div>
+            <div className="Ant2">f</div>
+          </div>
+        </div>
+        <div className="Santa">
+          <div className="cap">
+            <div className="captop"></div>
+            <div className="capmid"></div>
+            <div className="capbottom"></div>
+          </div>
+          <div className="Shead">
+            <div className="headbase">
+              <div className="headeyes">
+                <div className="Seye1"></div>
+                <div className="Seye2"></div>
+              </div>
+              <div className="headnose"></div>
+              <div className="beard"></div>
+            </div>
+          </div>
+          <div className="SBody">
+            <div className="Scoat">
+              <div className="Scoatin">
+                <div className="Sbtn"></div>
+                <div className="Sbtn"></div>
+                <div className="Sbtn"></div>
+                <div className="Sbtn"></div>
+                <div className="Sbtn"></div>
+                <div className="Sbtn"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="heading">
+          <h1>
+            <span className="bell1">&#128276;</span>Merry
+            <span className="bell2">&#128276;</span>
+          </h1>
+          <h1>Christmas</h1>
+        </div>
+      </div>
+    </>
+  );
+}
